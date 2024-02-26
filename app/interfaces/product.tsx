66f1e501bf1473b3c74914a5980751d0d065a13a
smooooky <75350537+smooooky@users.noreply.github.com>
2024-02-26 12:35:49 -0500

    added people interface, added people service

--- app/interfaces/product.tsx
+++ app/interfaces/product.tsx
@@ -1,8 +1,9 @@
 import { Planet } from "./planet";
 import { Ship } from "./ship";
 import { Vehicle } from "./vehicle";
+import { People } from "./people";
 
 // Define the product type
 export interface Product {
-  [key: string]: Planet | Ship | Vehicle | string | number;
+  [key: string]: Planet | Ship | People | Vehicle | string | number;
 }
